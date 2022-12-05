1dd4bfa277f622c1610115017d14b3a674d3a17d
Arne Martensen <arne.martensen@burohappold.com>
2022-12-05 15:53:46 +0100

    Push Of Nodes Works int RFEM6 works
    Push of Nodes in RFEM works. Double orrcing nodes are detected automatically and only one node will be added then.

---
+++ RFEM6_Adapter/CRUD/Read/Node.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using BH.oM.Adapter;
+using BH.oM.Structure.Elements;
+
+using rfModel = Dlubal.WS.Rfem6.Model;
+
+namespace BH.Adapter.RFEM6
+{
+    internal partial class RFEM6Adapter
+    {
+
+        private List<Node> ReadNodes(List<string> ids = null)
+        {
+
+            List<Node> nodeList = new List<Node>();
+
+            //if (ids == null)
+            //{
+            //    foreach (rfModel.node rfNode in rfModel.getno)
+            //    {
+            //        nodeList.Add(rfNode.FromRFEM());
+            //    }
+            //}
+            //else
+            //{
+            //    foreach (string id in ids)
+            //    {
+            //        nodeList.Add(modelData.GetNode(Int32.Parse(id), rf.ItemAt.AtNo).GetData().FromRFEM());
+            //    }
+            //}
+
+
+            return nodeList;
+        }
+
+    }
+}
