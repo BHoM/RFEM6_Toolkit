e201c2d00ca15747cc045896367d6967869f8f7e
Arne Martensen <arne.martensen@burohappold.com>
2022-12-06 16:18:51 +0100

    Push of Nodes and Constraints does work.

---
+++ RFEM6_Adapter/CRUD/Read/Constraint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using BH.oM.Adapter;
+using BH.oM.Structure.Elements;
+
+using rfModel = Dlubal.WS.Rfem6.Model;
+
+namespace BH.Adapter.RFEM6
+{
+    public partial class RFEM6Adapter
+    {
+
+        private List<Node> ReadConstraints(List<string> ids = null)
+        {
+
+            List<Node> constraintList = new List<Node>();
+
+
+
+     
+
+            return constraintList;
+        }
+
+    }
+}
