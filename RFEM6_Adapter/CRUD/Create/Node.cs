70b8372cfa39e3a836e279d2f3fcc75dff7d309d
Arne Martensen <arne.martensen@burohappold.com>
2022-12-06 16:18:51 +0100

    Push of Nodes and Constraints does work.

--- RFEM6_Adapter/CRUD/Create/Node.cs
+++ RFEM6_Adapter/CRUD/Create/Node.cs
@@ -24,6 +24,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Numerics;
 
 using BH.oM.Adapter;
 using BH.oM.Structure.Elements;
@@ -40,18 +41,16 @@ namespace BH.Adapter.RFEM6
             //NOTE:A geometric object has, in general, a parent_no = 0. The parent_no parameter becomes significant for example with loads.
             int nodeId = model.get_first_free_number(rfModel.object_types.E_OBJECT_TYPE_NODE, 0);
 
-
             int counter = 0;
-
 
             for (int i = 0; i < bhNodes.Count(); i++)
             {
                 Node bhNode = bhNodes.ToList()[i];
 
-                //bool nodeDoesExisitInModel = modelDoesContainNode(bhNode);
-
-                
-                if (modelDoesContainNode(bhNode)==null) {
+                rfModel.node rfNd= modelDoesContainNode(bhNode);
+               
+                //check if node does exist
+                if (rfNd == null) {
 
 
                     rfModel.node rfNode = new rfModel.node()
@@ -63,15 +62,42 @@ namespace BH.Adapter.RFEM6
                         comment = "concrete part"
                     };
                     model.set_node(rfNode);
+
                 }
-                else
+
+                //does node have a support?
+                if (bhNode.Support!=null)
                 {
-                     counter = counter +1;
 
+                    int nodalSupportId = model.get_first_free_number(rfModel.object_types.E_OBJECT_TYPE_NODAL_SUPPORT, 0);
+
+                    rfModel.node constraintLocationNode = modelDoesContainNode(bhNode);
+
+                    //check if support already exist
+                    var existinConstraint = checkForExistenceOfConstraint(bhNode.Support);
+                    if (existinConstraint == null)
+                    {
+                        rfModel.nodal_support rfNodalSuuuport = new rfModel.nodal_support()
+                        {
+                            no = nodalSupportId,
+                            nodes = new int[] { constraintLocationNode.no },
+                            spring = new rfModel.vector_3d() { x = stiffnessTranslationBHToRF("" + bhNode.Support.TranslationX), y = stiffnessTranslationBHToRF("" + bhNode.Support.TranslationY), z = stiffnessTranslationBHToRF("" + bhNode.Support.TranslationZ) },
+                            rotational_restraint = new rfModel.vector_3d() { x = stiffnessTranslationBHToRF("" + bhNode.Support.RotationX), y = stiffnessTranslationBHToRF("" + bhNode.Support.RotationY), z = stiffnessTranslationBHToRF("" + bhNode.Support.RotationZ) },
+                        };
+
+                        model.set_nodal_support(rfNodalSuuuport);
+                    }
+                    else
+                    {
+                        var a = modelDoesContainNode(bhNode);
+                        var b = existinConstraint.nodes.ToList();
+                        b.Add(a.no);
+                        existinConstraint.nodes=b.ToArray();
+                        model.delete_object(rfModel.object_types.E_OBJECT_TYPE_NODAL_SUPPORT, existinConstraint.no, 0);
+                        model.set_nodal_support(existinConstraint);
+                    }
                 }
-
             }
-
 
             return true;
         }
@@ -89,6 +115,40 @@ namespace BH.Adapter.RFEM6
                 return collectedNode.ToList().First(); 
             }
 
+            return null;
+        }
+
+        private Double stiffnessTranslationBHToRF(String stiffness)
+        {
+
+            Double result = stiffness=="Free"?0.0:Double.PositiveInfinity;
+
+            return result;
+        }
+
+        private rfModel.nodal_support checkForExistenceOfConstraint(BH.oM.Structure.Constraints.Constraint6DOF constraint)
+        {
+
+            //gettin all nodalsupport from model
+            var numbers = model.get_all_object_numbers_by_type(rfModel.object_types.E_OBJECT_TYPE_NODAL_SUPPORT);
+            var allSupports = numbers.ToList().Select(n => model.get_nodal_support(n.no));
+
+            //check if there is one support in model that does fullfill all criterial for equality
+            foreach (var s in allSupports)
+            {
+               bool x = s.spring.x.Equals(stiffnessTranslationBHToRF("" + constraint.TranslationX));
+               bool y = s.spring.y.Equals(stiffnessTranslationBHToRF("" + constraint.TranslationY));
+               bool z = s.spring.z.Equals(stiffnessTranslationBHToRF("" + constraint.TranslationZ));
+               bool xx = s.rotational_restraint.x.Equals(stiffnessTranslationBHToRF("" + constraint.RotationX));
+               bool yy = s.rotational_restraint.y.Equals(stiffnessTranslationBHToRF("" + constraint.RotationY));
+               bool zz = s.rotational_restraint.z.Equals(stiffnessTranslationBHToRF("" + constraint.RotationZ));
+
+                if (x&&y&&z&&xx&&yy&&zz)
+                {
+                    return s;
+                }
+
+            }
 
             return null;
         }
