eef6252ed3da53fb445c79da2167a30a847f400e
Arne Martensen <arne.martensen@burohappold.com>
2023-01-04 10:06:58 +0100

    Create SectionProperties.cs

---
+++ RFEM6_Adapter/CRUD/Create/SectionProperties.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using BH.oM.Adapter;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.MaterialFragments;
+using BH.oM.Structure.SectionProperties;
+
+using rfModel = Dlubal.WS.Rfem6.Model;
+
+namespace BH.Adapter.RFEM6
+{
+    public partial class RFEM6Adapter
+    {
+
+        //Has been implemented inside of Nodes.cs
+
+        private bool CreateCollection(IEnumerable<ISectionProperty> sectionProperties)
+        {
+
+
+
+            return true;
+
+        }
+
+    }
+}
