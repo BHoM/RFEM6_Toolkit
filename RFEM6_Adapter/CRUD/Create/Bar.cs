06359ab325a7ad6582e2818508df6b4195bf27eb
Arne Martensen <arne.martensen@burohappold.com>
2023-01-16 14:14:50 +0100

    Push of LIne works

--- RFEM6_Adapter/CRUD/Create/Bar.cs
+++ RFEM6_Adapter/CRUD/Create/Bar.cs
@@ -13,11 +13,39 @@ namespace BH.Adapter.RFEM6
     public partial class RFEM6Adapter
     {
 
-        private bool CreateCollection(IEnumerable<Bar> bhBar)
+        private bool CreateCollection(IEnumerable<Bar> bhBars)
         {
 
-            //Create Lines
 
+            foreach (Bar bhBar in bhBars) {
+
+                //Find Corresponding Nodes
+                rfModel.object_with_children[] rfNodesByType =  model.get_all_object_numbers_by_type(rfModel.object_types.E_OBJECT_TYPE_MATERIAL);
+                IEnumerable<int> rfNodeNumbers = rfNodesByType.ToList().Select(m => m.no);
+
+                List<rfModel.node> nodeList = new List<rfModel.node>();
+
+                rfModel.node start = getNodeFromRFModel(bhBar.StartNode);
+                rfModel.node end = getNodeFromRFModel(bhBar.EndNode);
+
+                if (start!=null && end!=null) { 
+                
+                    //rfModel.line rfLine = new rfModel.line();
+
+                    rfModel.line rfLine = new rfModel.line()
+                    {
+                        no = 1,
+                        definition_nodes = new int[] { start.no, end.no },
+                        type = rfModel.line_type.TYPE_POLYLINE,
+                    };
+
+                    model.set_line(rfLine);
+                }
+
+            }
+
+            //Create Lines
+            
 
             //Create Material
             //Create Sections
--- RFEM6_Adapter/Convert/ToRFEM6/SectionProperties.cs
+++ RFEM6_Adapter/Convert/ToRFEM6/SectionProperties.cs
@@ -19,6 +19,27 @@ namespace BH.Adapter.RFEM6
         public static rfModel.section ToRFEM6(this ISectionProperty bhSection, int secNo, int matNo )
         {
 
+
+            if (bhSection.Name.Split(' ')[0].Equals("L")) {
+
+                int v0 = (int)Double.Parse(bhSection.Name.Split(' ').ToArray().Last().Split('x').ToArray()[0]);
+                int v1 = (int)Double.Parse(bhSection.Name.Split(' ').ToArray().Last().Split('x').ToArray()[1]);
+                double thickness = Double.Parse(bhSection.Name.Split(' ').ToArray().Last().Split('x').ToArray().Last());
+
+                bhSection.Name = $"L {v0}x{v1}x{thickness}";
+
+            } else if (!bhSection.Name.Substring(3, 1).Equals(" ")&&((bhSection.Name.Substring(0, 3).Equals("RHS")) || (bhSection.Name.Substring(0, 3).Equals("SHS"))))
+            {
+                bhSection.Name = bhSection.Name.Insert(3, " ");
+
+            } else if (bhSection.Name.Substring(0, 3).Equals("TUB") || bhSection.Name.Substring(0, 3).Equals("TUC"))
+            {
+
+                bhSection.Name = bhSection.Name.Remove(0,1);
+                bhSection.Name = bhSection.Name.Insert(0, "1/2 ");
+
+            }
+
             // create section
             rfModel.section rfSection = new rfModel.section
             {
