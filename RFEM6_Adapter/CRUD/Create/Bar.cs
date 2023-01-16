23e6817780be57c134a00c9e8c71939cfda80490
Arne Martensen <arne.martensen@burohappold.com>
2023-01-16 17:24:39 +0100

    Push of Bars does work now.

--- RFEM6_Adapter/CRUD/Create/Bar.cs
+++ RFEM6_Adapter/CRUD/Create/Bar.cs
@@ -5,6 +5,8 @@ using System.Text;
 
 using BH.oM.Adapter;
 using BH.oM.Structure.Elements;
+using BH.oM.Structure.MaterialFragments;
+using BH.oM.Structure.SectionProperties;
 
 using rfModel = Dlubal.WS.Rfem6.Model;
 
@@ -32,14 +34,40 @@ namespace BH.Adapter.RFEM6
                 
                     //rfModel.line rfLine = new rfModel.line();
 
-                    rfModel.line rfLine = new rfModel.line()
-                    {
-                        no = 1,
-                        definition_nodes = new int[] { start.no, end.no },
-                        type = rfModel.line_type.TYPE_POLYLINE,
-                    };
+                   
+                   
 
-                    model.set_line(rfLine);
+
+                    if (!lineDoesExist(start,end)) {
+
+                        rfModel.line rfLine = new rfModel.line()
+                        {
+                            no = model.get_first_free_number(rfModel.object_types.E_OBJECT_TYPE_LINE, 0),
+                            definition_nodes = new int[] { start.no, end.no },
+                            type = rfModel.line_type.TYPE_POLYLINE,
+                        };
+
+                        model.set_line(rfLine);
+
+                        rfModel.member rfMember = new rfModel.member()
+                        {
+                            no = model.get_first_free_number(rfModel.object_types.E_OBJECT_TYPE_MEMBER, 0),
+                            line = rfLine.no,
+                            lineSpecified = true,
+                            section_start = getRFSection( bhBar.SectionProperty).no,
+                            section_startSpecified = true,
+                            //section_end = sectionSquare.no,
+                            section_endSpecified = true,
+                            comment = "Arnes First Beam",
+                        };
+
+                        model.set_member(rfMember);
+
+                    }
+
+                    
+
+
                 }
 
             }
@@ -55,5 +83,55 @@ namespace BH.Adapter.RFEM6
             return true;
         }
 
+
+        private bool lineDoesExist(rfModel.node n0, rfModel.node n1) {
+
+            rfModel.object_with_children[] rfLineByType = model.get_all_object_numbers_by_type(rfModel.object_types.E_OBJECT_TYPE_LINE);
+            IEnumerable<int> rfLineNumbers = rfLineByType.ToList().Select(m => m.no);
+
+            foreach (int l in rfLineNumbers) {
+
+
+                rfModel.line currentLine=model.get_line(l);
+
+                HashSet<int> nodeIds = currentLine.definition_nodes.ToHashSet();
+
+
+                if (nodeIds.Contains(n0.no)&& nodeIds.Contains(n1.no)) { 
+
+                    return true;
+
+                }
+
+            }
+
+            return false;
+        }
+
+        private rfModel.section getRFSection(ISectionProperty bhSection) {
+
+
+            rfModel.object_with_children[] rfSectionsByType = model.get_all_object_numbers_by_type(rfModel.object_types.E_OBJECT_TYPE_SECTION);
+            IEnumerable<int> rfSectionNumbers = rfSectionsByType.ToList().Select(m => m.no);
+            string bhSectionName= String.Concat(bhSection.Name.Where(c => !Char.IsWhiteSpace(c))).Split('|')[0].ToUpper();
+
+            foreach (int n in rfSectionNumbers) {
+
+                rfModel.section currSection=model.get_section(n);
+
+                string rfSectionName = String.Concat(currSection.name.Where(c => !Char.IsWhiteSpace(c))).Split('|')[0].ToUpper(); ;
+
+                if (rfSectionName.Equals(bhSectionName))
+                {
+                    return model.get_section(n);
+                }
+
+            }
+
+            return null;
+
+
+
+        }
     }
 }
