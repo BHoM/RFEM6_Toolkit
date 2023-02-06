8ce41ab569392f44cc36f499aa9d44d89429f825
Arne Martensen <arne.martensen@burohappold.com>
2023-02-06 16:46:14 +0100

    clean up

--- RFEM6_Adapter/Convert/ToRFEM6/Material.cs
+++ RFEM6_Adapter/Convert/ToRFEM6/Material.cs
@@ -19,8 +19,6 @@ namespace BH.Adapter.RFEM6
 
         public static rfModel.material ToRFEM6(IMaterialFragment bhMateraial, int materialNo)
         {
-
-
             //Object[] nameAndType = materialTypeAndNameTranslater(bhMateraial);
 
             rfModel.material rfMaterial = new rfModel.material
@@ -29,65 +27,15 @@ namespace BH.Adapter.RFEM6
                 name = bhMateraial.Name,
                 //name = "S235 (DIN EN 1993-1-1:2010-12)",
                 //material_type = rfModel.material_material_type.TYPE_STEEL,
-                comment = "" ,
-                material_type = bhMateraial.GetType().Name.Equals("Steel") ? rfModel.material_material_type.TYPE_STEEL:rfModel.material_material_type.TYPE_CONCRETE
+                comment = "",
+                material_type = bhMateraial.GetType().Name.Equals("Steel") ? rfModel.material_material_type.TYPE_STEEL : rfModel.material_material_type.TYPE_CONCRETE
             };
-
 
             return rfMaterial;
 
         }
 
-        private static Object[] materialTypeAndNameTranslater(IMaterialFragment bhMaterial)
-        {
-
-
-            Object[] result = new Object[3];
-
-            if (bhMaterial.GetType().Equals(typeof(Steel)))
-            {
-                result[0] = bhMaterial.Name;
-                result[1] = rfModel.material_material_type.TYPE_STEEL;
-                result[2] = bhMaterial.Name + "|" +
-                    BH.Engine.Base.Query.PropertyValue(bhMaterial, "Density") + "|" +
-                    BH.Engine.Base.Query.PropertyValue(bhMaterial, "DampingRatio") + "|" +
-                    BH.Engine.Base.Query.PropertyValue(bhMaterial, "PoissonsRatio") + "|" +
-                    BH.Engine.Base.Query.PropertyValue(bhMaterial, "ThermalExpansionCoeff") + "|" +
-                    BH.Engine.Base.Query.PropertyValue(bhMaterial, "YoungsModulus") + "|" +
-                    BH.Engine.Base.Query.PropertyValue(bhMaterial, "YieldStress") + "|" +
-                    BH.Engine.Base.Query.PropertyValue(bhMaterial, "UltimateStress");
-            }
-            else if (bhMaterial.GetType().Equals(typeof(Concrete)))
-            {
-                result[0] = bhMaterial.Name;
-                result[1] = rfModel.material_material_type.TYPE_CONCRETE;
-                result[2] = bhMaterial.Name + "|" +
-                BH.Engine.Base.Query.PropertyValue(bhMaterial, "Density") + "|" +
-                BH.Engine.Base.Query.PropertyValue(bhMaterial, "DampingRatio") + "|" +
-                BH.Engine.Base.Query.PropertyValue(bhMaterial, "PoissonsRatio") + "|" +
-                BH.Engine.Base.Query.PropertyValue(bhMaterial, "ThermalExpansionCoeff") + "|" +
-                BH.Engine.Base.Query.PropertyValue(bhMaterial, "YoungsModulus") + "|" +
-                BH.Engine.Base.Query.PropertyValue(bhMaterial, "CylinderStrength") + "|" +
-                BH.Engine.Base.Query.PropertyValue(bhMaterial, "CubeStrength");
-            }
-            else
-            {
-                Engine.Base.Compute.RecordWarning("The Material " + bhMaterial + " has not been implemented yet. Steel S235 has been created insted.");
-                result[0] = "S235";
-                result[1] = rfModel.material_material_type.TYPE_STEEL;
-                result[2] = bhMaterial.Name + "|" +
-                7850 + "|" +
-                0 + "|" +
-                0.3 + "|" +
-                0.000012 + "|" +
-                2.1e+11 + "|" +
-                2.35e+8 + "|" +
-                3.6e+8;
-
-            }
-
-            return result;
-        }
+   
 
     }
 }
