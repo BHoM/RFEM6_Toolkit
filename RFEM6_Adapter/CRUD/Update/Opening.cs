f990a3bcce8797cd185ad2fa82951f733b1c070e
Arne Martensen <arne.martensen@burohappold.com>
2023-04-19 10:41:31 +0200

    Exploring: Cahnge of Update Method

--- RFEM6_Adapter/CRUD/Update/Opening.cs
+++ RFEM6_Adapter/CRUD/Update/Opening.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * This file is part of the Buildings and Habitats object Model (BHoM)
  * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
  *
@@ -19,47 +19,36 @@
  * You should have received a copy of the GNU Lesser General Public License     
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
-
-using BH.oM.Adapter;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using BH.oM.Base;
+
+using BH.oM.Adapter;
+using BH.oM.Structure.Elements;
+using BH.oM.Adapters.RFEM6;
+
+using rfModel = Dlubal.WS.Rfem6.Model;
 
 namespace BH.Adapter.RFEM6
 {
     public partial class RFEM6Adapter : BHoMAdapter
     {
-        // This method gets called when appropriate by the Push method contained in the base Adapter class.
-        // Unlike the Create, Delete and Read, this method already exposes a simple implementation: it calls Delete and then Create.
-        // It can be overridden here keeping in mind the following:
-        // - it gets called once per each Type, and if equal objects are found;
-        // - the object equality is tested through this.AdapterComparers, that need to be implemented for each type.
-        // See the wiki for more info.
-
-
-        protected override bool IUpdate<T>(IEnumerable<T> objects, ActionConfig actionConfig = null)
-        {
-            //return ICreate<T>(objects, actionConfig);
-            return UpdateObjects(objects as dynamic);
-        }
-
+        /***************************************************/
+        /**** Update Node                               ****/
         /***************************************************/
 
-        private bool UpdateObjects<T>(IEnumerable<T> objects) where T : IBHoMObject
+        private bool UpdateObjects(IEnumerable<Opening> openings)
         {
-            return base.IUpdate(objects, null);
-        }
+            bool success = true;
 
-        /***************************************************/
+            foreach (Opening opening in openings)
+            {
+                m_Model.set_opening(opening.ToRFEM6());
+            }
+
+            return success;
+        }
 
     }
-
-
-    /***************************************************/
-
-
 }
-
