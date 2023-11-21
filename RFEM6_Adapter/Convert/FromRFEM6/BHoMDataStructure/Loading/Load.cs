d9083bb03858fec58a608595aa7cff34aa5fde3f
Arne Martensen <arne.martensen@burohappold.com>
2023-11-21 11:06:23 +0100

    Direction tested pull bug resolved

--- RFEM6_Adapter/Convert/FromRFEM6/BHoMDataStructure/Loading/Load.cs
+++ RFEM6_Adapter/Convert/FromRFEM6/BHoMDataStructure/Loading/Load.cs
@@ -42,13 +42,32 @@ namespace BH.Adapter.RFEM6
 
         public static BarUniformlyDistributedLoad FromRFEM(this rfModel.member_load rfMemberLoad, List<Bar> bhBars, Loadcase bhLoadCase)
         {
+            Vector forceVector = new Vector() { X = 0, Y = 0, Z = 0 };
+            Vector momentVector = new Vector() { X = 0, Y = 0, Z = 0 };
 
+            if (rfMemberLoad.load_direction == member_load_load_direction.LOAD_DIRECTION_GLOBAL_X_OR_USER_DEFINED_U_PROJECTED || rfMemberLoad.load_direction == member_load_load_direction.LOAD_DIRECTION_GLOBAL_X_OR_USER_DEFINED_U_TRUE || rfMemberLoad.load_direction == member_load_load_direction.LOAD_DIRECTION_LOCAL_X)
+            {
+                momentVector = rfMemberLoad.load_type == member_load_load_type.LOAD_TYPE_MOMENT ? new Vector() { X =rfMemberLoad.magnitude, Y = 0, Z = 0 } : new Vector() { X = 0, Y = 0, Z = 0 };
+                forceVector = rfMemberLoad.load_type == member_load_load_type.LOAD_TYPE_FORCE ? new Vector() { X = rfMemberLoad.magnitude, Y = 0, Z = 0 } : new Vector() { X = 0, Y = 0, Z = 0 };
+
+            }
+            else if (rfMemberLoad.load_direction == member_load_load_direction.LOAD_DIRECTION_GLOBAL_Y_OR_USER_DEFINED_V_PROJECTED || rfMemberLoad.load_direction == member_load_load_direction.LOAD_DIRECTION_GLOBAL_Y_OR_USER_DEFINED_V_TRUE || rfMemberLoad.load_direction == member_load_load_direction.LOAD_DIRECTION_LOCAL_Y)
+            {
+                momentVector = rfMemberLoad.load_type == member_load_load_type.LOAD_TYPE_MOMENT ? new Vector() { X = 0, Y = rfMemberLoad.magnitude, Z = 0 } :  new Vector() { X = 0, Y = 0, Z = 0 };
+                forceVector = rfMemberLoad.load_type == member_load_load_type.LOAD_TYPE_FORCE ? new Vector() { X = 0, Y = rfMemberLoad.magnitude, Z = 0 } : new Vector() { X = 0, Y = 0, Z = 0 };
+            }
+            else
+            {
+                momentVector = rfMemberLoad.load_type == member_load_load_type.LOAD_TYPE_MOMENT ?  new Vector() { X = 0, Y = 0, Z = rfMemberLoad.magnitude } :  new Vector() { X = 0, Y = 0, Z = 0 };
+                forceVector = rfMemberLoad.load_type == member_load_load_type.LOAD_TYPE_FORCE ? new Vector() { X = 0, Y = 0, Z = rfMemberLoad.magnitude } : new Vector() { X = 0, Y = 0, Z = 0 };
+            }
 
             BarUniformlyDistributedLoad bhLoad = new BarUniformlyDistributedLoad
             {
                 Objects = new BH.oM.Base.BHoMGroup<Bar>() { Elements = bhBars },
                 Loadcase = bhLoadCase,
-                Force = BH.Engine.Geometry.Create.Vector(0, 0, rfMemberLoad.magnitude),
+                Force = forceVector,
+                Moment = momentVector
             };
 
             return bhLoad;
@@ -85,8 +104,8 @@ namespace BH.Adapter.RFEM6
             {
                 Objects = new BH.oM.Base.BHoMGroup<Node>() { Elements = bhNodes },
                 Loadcase = bhLoadCase,
-                Force = BH.Engine.Geometry.Create.Vector(force[0], -force[1], -force[2]),
-                Moment = BH.Engine.Geometry.Create.Vector(moment[0], -moment[1], -moment[2]),
+                Force = BH.Engine.Geometry.Create.Vector(force[0], force[1], force[2]),
+                Moment = BH.Engine.Geometry.Create.Vector(moment[0], moment[1], moment[2]),
             };
 
 
