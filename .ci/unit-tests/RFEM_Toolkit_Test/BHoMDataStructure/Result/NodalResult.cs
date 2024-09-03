591f7e4d9fa7c200e5fc02e3fb60f155fb0cc550
Arne Martensen <arne.martensen@burohappold.com>
2024-09-03 16:25:54 +0200

    Package Update/Pull NodalResults Updated

--- .ci/unit-tests/RFEM_Toolkit_Test/BHoMDataStructure/Result/NodalResult.cs
+++ .ci/unit-tests/RFEM_Toolkit_Test/BHoMDataStructure/Result/NodalResult.cs
@@ -27,63 +27,88 @@ using BH.Engine.Structure;
 using BH.oM.Structure.SectionProperties;
 using BH.oM.Geometry;
 using BH.oM.Structure.Elements;
+using BH.oM.Analytical.Elements;
+using BH.oM.Base;
+using BH.oM.Structure.Requests;
 
 namespace RFEM_Toolkit_Test.Elements
 {
 
 
-    public class NodalResultTestClass
+	public class NodalResultTestClass
 
-    {
+	{
 
-        RFEM6Adapter adapter;
+		RFEM6Adapter adapter;
 
-        Line line0;
-        Line line1;
-        Line line2;
-        Line line3;
-
-
-
-        IMaterialFragment glulam;
-        IMaterialFragment timberC;
-        //IMaterialFragment timberT;
-        //IMaterialFragment timberD;
-
-        Concrete concrete;
-        //IMaterialFragment steel;
-
-        ISectionProperty steelSection;
-        ISectionProperty concreteSection;
-        ISectionProperty genericSectionGLTimber;
-        ISectionProperty genericSectionSawnTimber;
-
-        Bar barSteelSection;
-        Bar barConcreteSection;
-        Bar barGLTimber;
-        Bar barSawTimber;
-
-        BarEndNodesDistanceComparer comparer = new BarEndNodesDistanceComparer(3);
-
-        [TearDown]
-        public void TearDown()
-        {
-            adapter.Wipeout();
-        }
-
-        [OneTimeSetUp]
-        public void InitializeRFEM6Adapter()
-        {
-            adapter = new RFEM6Adapter(true);
-
-            
-            
-
-        }
+		Line line0;
+		Line line1;
+		Line line2;
+		Line line3;
 
 
-      
+
+		IMaterialFragment glulam;
+		IMaterialFragment timberC;
+		//IMaterialFragment timberT;
+		//IMaterialFragment timberD;
+
+		Concrete concrete;
+		//IMaterialFragment steel;
+
+		ISectionProperty steelSection;
+		ISectionProperty concreteSection;
+		ISectionProperty genericSectionGLTimber;
+		ISectionProperty genericSectionSawnTimber;
+
+		Bar barSteelSection;
+		Bar barConcreteSection;
+		Bar barGLTimber;
+		Bar barSawTimber;
+
+		BarEndNodesDistanceComparer comparer = new BarEndNodesDistanceComparer(3);
+
+		[TearDown]
+		public void TearDown()
+		{
+			adapter.Wipeout();
+		}
+
+		[OneTimeSetUp]
+		public void InitializeRFEM6Adapter()
+		{
+			adapter = new RFEM6Adapter(true);
 
 
-    }
+
+
+		}
+
+		[Test]
+		public void ReadResult()
+		{
+
+			NodeResultRequest request = new NodeResultRequest();
+			request.Cases = new List<Object> { 1 };
+			request.ObjectIds = new List<object> { 1, 2, 3, 4, 5 ,6};
+			request.ResultType = NodeResultType.NodeReaction;
+
+			//Push it once
+			var obj = adapter.Pull(request);
+
+			obj.First();
+
+			////Pull it
+			//FilterRequest edgeFilter = new FilterRequest() { Type = typeof(Edge) };
+			//var edgePulled = adapter.Pull(edgeFilter).ToList();
+			//Edge ep = (Edge)edgePulled[0];
+
+			////Check
+			//Assert.IsNotNull(ep);
+			//Assert.True(comparer.Equals(edge1, ep));
+		}
+
+
+
+	}
 }
