31f528af5c21f63ad6748c28c62edce85d992797
Arne Martensen <arne.martensen@burohappold.com>
2025-01-23 17:01:47 +0100

    Extremes for Bar Forces Seem To work

--- RFEM6_Adapter/CRUD/Read/BHoMDataStructure/Result/BarResults.cs
+++ RFEM6_Adapter/CRUD/Read/BHoMDataStructure/Result/BarResults.cs
@@ -39,6 +39,8 @@ using BH.oM.Structure.Requests;
 using BH.oM.Structure.Results;
 using System.Configuration;
 using System.Globalization;
+using System.ComponentModel;
+using System.Linq.Expressions;
 
 namespace BH.Adapter.RFEM6
 {
@@ -48,7 +50,7 @@ namespace BH.Adapter.RFEM6
 		public IEnumerable<IResult> ReadResults(BarResultRequest request, ActionConfig actionConfig)
 		{
 
-			BH.Engine.Base.Compute.RecordWarning($"Divisions are set to {request.Divisions}. Division functionality has not been implemented yet in RFEM6_Toolkit. Currently, the number of divisions depends solely on what RFEM6 provides and can vary significantly based on the load type on the corresponding Bar/Member.");
+			BH.Engine.Base.Compute.RecordWarning($"Divisions are set to {request.Divisions}. Division functionality has not been implemented yet in RFEM6_Toolkit. Currently, the number of divisions depends solely on what RFEM6 provides and can vary significantly based on the load type on the corresponding Bar/Member and the DivisionType.");
 
 			m_Model.use_detailed_member_results(true);
 			// Loading of Member And LoadCase Ids
@@ -65,7 +67,36 @@ namespace BH.Adapter.RFEM6
 				//Get Results bar forces for specific LC
 				members_internal_forces_row[] resultForMemberInternalForces = m_Model.get_results_for_members_internal_forces(case_object_types.E_OBJECT_TYPE_LOAD_CASE, c, filters, axes_type.MEMBER_AXES);
 
+				///////////////////////////////////////////playground
 
+				INotifyPropertyChanged[] barResults = m_Model.get_results_for_members_internal_forces(case_object_types.E_OBJECT_TYPE_LOAD_CASE, c, filters, axes_type.MEMBER_AXES);
+
+				switch (request.ResultType)
+				{
+
+					case BarResultType.BarForce:
+						barResults = m_Model.get_results_for_members_internal_forces(case_object_types.E_OBJECT_TYPE_LOAD_CASE, c, filters, axes_type.MEMBER_AXES);
+						break;
+					case BarResultType.BarDisplacement:
+						barResults = m_Model.get_results_for_members_local_deformations(case_object_types.E_OBJECT_TYPE_LOAD_CASE, c, filters, axes_type.MEMBER_AXES);
+						break;
+					case BarResultType.BarDeformation:
+						barResults = m_Model.get_results_for_members_global_deformations(case_object_types.E_OBJECT_TYPE_LOAD_CASE, c, filters);
+						break;
+					case BarResultType.BarStrain:
+						barResults = m_Model.get_results_for_members_strains(case_object_types.E_OBJECT_TYPE_LOAD_CASE, c, filters, axes_type.MEMBER_AXES);
+						break;
+					case BarResultType.BarStress:
+
+						BH.Engine.Base.Compute.RecordError("The Pull of Bar Stresses has not been developed Yet");
+						return null;
+						break;
+
+					default:
+						barResults = m_Model.get_results_for_members_internal_forces(case_object_types.E_OBJECT_TYPE_LOAD_CASE, c, filters, axes_type.MEMBER_AXES);
+						break;
+
+				}
 
 				//Grouping of Internal Forces grouped by Member No
 				var memberInternalForceGroup = resultForMemberInternalForces.GroupBy(r => r.row.member_no);
@@ -84,47 +115,8 @@ namespace BH.Adapter.RFEM6
 					if (request.DivisionType == DivisionType.ExtremeValues)
 					{
 
-						var extremes_ = member.ToList().TakeWhile(v => !v.description.Contains("Extreme")).Aggregate(
-			   new
-			   {
-				   NMaxSection = member.First(),
-				   NMinSection = member.First(),
-				   ZMaxSection = member.First(),
-				   ZMinSection = member.First(),
-				   YMaxSection = member.First(),
-				   YMinSection = member.First(),
-				   MXMaxSection = member.First(),
-				   MXMinSection = member.First(),
-				   MYMaxSection = member.First(),
-				   MYMinSection = member.First(),
-				   MZMaxSection = member.First(),
-				   MZMinSection = member.First()
-			   },
-		(acc, m) => new
-		{
-			NMaxSection = m.row.internal_force_n > acc.NMaxSection.row.internal_force_n ? m : acc.NMaxSection,
-			NMinSection = m.row.internal_force_n < acc.NMinSection.row.internal_force_n ? m : acc.NMinSection,
-			ZMaxSection = m.row.internal_force_vz > acc.ZMaxSection.row.internal_force_vz ? m : acc.ZMaxSection,
-			ZMinSection = m.row.internal_force_vz < acc.ZMinSection.row.internal_force_vz ? m : acc.ZMinSection,
-			YMaxSection = m.row.internal_force_vy > acc.YMaxSection.row.internal_force_vy ? m : acc.YMaxSection,
-			YMinSection = m.row.internal_force_vy < acc.YMinSection.row.internal_force_vy ? m : acc.YMinSection,
-			MXMaxSection = m.row.internal_force_mt > acc.MXMaxSection.row.internal_force_mt ? m : acc.MXMaxSection,
-			MXMinSection = m.row.internal_force_mt < acc.MXMinSection.row.internal_force_mt ? m : acc.MXMinSection,
-			MYMaxSection = m.row.internal_force_my > acc.MYMaxSection.row.internal_force_my ? m : acc.MYMaxSection,
-			MYMinSection = m.row.internal_force_my < acc.MYMinSection.row.internal_force_my ? m : acc.MYMinSection,
-			MZMaxSection = m.row.internal_force_mz > acc.MZMaxSection.row.internal_force_mz ? m : acc.MZMaxSection,
-			MZMinSection = m.row.internal_force_mz < acc.MZMinSection.row.internal_force_mz ? m : acc.MZMinSection
-		}
-			);
+						List<members_internal_forces_row> extremeValues = getExtremesForBarForces(member.ToList());
 
-						List<members_internal_forces_row> extremeValues = new List<members_internal_forces_row>() {
-	extremes_.NMaxSection, extremes_.NMinSection,
-	extremes_.ZMaxSection, extremes_.ZMinSection,
-	extremes_.YMaxSection, extremes_.YMinSection,
-	extremes_.MXMaxSection, extremes_.MXMinSection,
-	extremes_.MYMaxSection, extremes_.MYMinSection,
-	extremes_.MZMaxSection, extremes_.MZMinSection
-};
 						foreach (var e in extremeValues)
 						{
 							//e.FromRFEM(c,memberLength).
@@ -153,6 +145,104 @@ namespace BH.Adapter.RFEM6
 			}
 
 			return allInternalForces;
+
+		}
+
+
+		private List<members_internal_forces_row> getExtremesForBarForces(List<members_internal_forces_row> membersInternalForces)
+		{
+
+			var extremes_ = membersInternalForces.ToList().TakeWhile(v => !v.description.Contains("Extreme")).Aggregate(
+			   new
+			   {
+				   NMaxSection = membersInternalForces.First(),
+				   NMinSection = membersInternalForces.First(),
+				   ZMaxSection = membersInternalForces.First(),
+				   ZMinSection = membersInternalForces.First(),
+				   YMaxSection = membersInternalForces.First(),
+				   YMinSection = membersInternalForces.First(),
+				   MXMaxSection = membersInternalForces.First(),
+				   MXMinSection = membersInternalForces.First(),
+				   MYMaxSection = membersInternalForces.First(),
+				   MYMinSection = membersInternalForces.First(),
+				   MZMaxSection = membersInternalForces.First(),
+				   MZMinSection = membersInternalForces.First()
+			   },
+		(acc, m) => new
+		{
+			NMaxSection = m.row.internal_force_n > acc.NMaxSection.row.internal_force_n ? m : acc.NMaxSection,
+			NMinSection = m.row.internal_force_n < acc.NMinSection.row.internal_force_n ? m : acc.NMinSection,
+			ZMaxSection = m.row.internal_force_vz > acc.ZMaxSection.row.internal_force_vz ? m : acc.ZMaxSection,
+			ZMinSection = m.row.internal_force_vz < acc.ZMinSection.row.internal_force_vz ? m : acc.ZMinSection,
+			YMaxSection = m.row.internal_force_vy > acc.YMaxSection.row.internal_force_vy ? m : acc.YMaxSection,
+			YMinSection = m.row.internal_force_vy < acc.YMinSection.row.internal_force_vy ? m : acc.YMinSection,
+			MXMaxSection = m.row.internal_force_mt > acc.MXMaxSection.row.internal_force_mt ? m : acc.MXMaxSection,
+			MXMinSection = m.row.internal_force_mt < acc.MXMinSection.row.internal_force_mt ? m : acc.MXMinSection,
+			MYMaxSection = m.row.internal_force_my > acc.MYMaxSection.row.internal_force_my ? m : acc.MYMaxSection,
+			MYMinSection = m.row.internal_force_my < acc.MYMinSection.row.internal_force_my ? m : acc.MYMinSection,
+			MZMaxSection = m.row.internal_force_mz > acc.MZMaxSection.row.internal_force_mz ? m : acc.MZMaxSection,
+			MZMinSection = m.row.internal_force_mz < acc.MZMinSection.row.internal_force_mz ? m : acc.MZMinSection
+		}
+			);
+
+			List<members_internal_forces_row> resultList = new List<members_internal_forces_row>() {
+	extremes_.NMaxSection, extremes_.NMinSection,
+	extremes_.YMaxSection, extremes_.YMinSection,
+	extremes_.ZMaxSection, extremes_.ZMinSection,
+	extremes_.MXMaxSection, extremes_.MXMinSection,
+	extremes_.MYMaxSection, extremes_.MYMinSection,
+	extremes_.MZMaxSection, extremes_.MZMinSection };
+
+			return resultList;
+
+		}
+		private List<members_local_deformations_row> getExtremesForBarForces(List<members_local_deformations_row> membersLocalDeformation)
+		{
+
+			var extremes_ = membersLocalDeformation.ToList().TakeWhile(v => !v.description.Contains("Extreme")).Aggregate(
+			   new
+			   {
+				   dXMax = membersLocalDeformation.First(),
+				   dXMin = membersLocalDeformation.First(),
+				   dyMax = membersLocalDeformation.First(),
+				   dyMin = membersLocalDeformation.First(),
+				   dzMax = membersLocalDeformation.First(),
+				   dzMin = membersLocalDeformation.First(),
+				   rotXMax = membersLocalDeformation.First(),
+				   rotXMin = membersLocalDeformation.First(),
+				   rotYMax = membersLocalDeformation.First(),
+				   rotYMin = membersLocalDeformation.First(),
+				   rotZMax = membersLocalDeformation.First(),
+				   rotZMin = membersLocalDeformation.First(),
+
+			   },
+(acc, m) => new
+{
+	dXMax = m.row.displacement_x > acc.dXMax.row.displacement_x ? m : acc.dXMax,
+	dXMin = m.row.displacement_x < acc.dXMin.row.displacement_x ? m : acc.dXMin,
+	dyMax = m.row.displacement_y > acc.dyMax.row.displacement_y ? m : acc.dyMax,
+	dyMin = m.row.displacement_y < acc.dyMin.row.displacement_y ? m : acc.dyMin,
+	dzMax = m.row.displacement_z > acc.dzMax.row.displacement_z ? m : acc.dzMax,
+	dzMin = m.row.displacement_z < acc.dzMin.row.displacement_z ? m : acc.dzMin,
+	rotXMax = m.row.rotation_x > acc.rotXMax.row.rotation_x ? m : acc.rotXMax,
+	rotXMin = m.row.rotation_x < acc.rotXMin.row.rotation_x ? m : acc.rotXMin,
+	rotYMax = m.row.rotation_y > acc.rotYMax.row.rotation_y ? m : acc.rotYMax,
+	rotYMin = m.row.rotation_y < acc.rotYMin.row.rotation_y ? m : acc.rotYMin,
+	rotZMax = m.row.rotation_z > acc.rotZMax.row.rotation_z ? m : acc.rotZMax,
+	rotZMin = m.row.rotation_z < acc.rotZMin.row.rotation_z ? m : acc.rotZMin
+}
+			);
+
+			List<members_local_deformations_row> resultList = new List<members_local_deformations_row>() {
+	extremes_.dXMax, extremes_.dXMin,
+	extremes_.dyMax, extremes_.dyMin,
+	extremes_.dzMax, extremes_.dzMin,
+	extremes_.rotXMax, extremes_.rotXMin,
+	extremes_.rotYMax, extremes_.rotYMin,
+	extremes_.rotZMax, extremes_.rotZMin
+};
+
+			return resultList;
 
 		}
 
