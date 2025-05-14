7812d65ea2f9cd26c91ef2be36a20b62e5d66757
Arne Martensen <arne.martensen@burohappold.com>
2025-05-14 14:04:58 +0200

    Uncomment to test if copyright error warning disappears

--- RFEM6_Adapter/CRUD/Create/BHoMDataStructure/Loading/LoadCombination.cs
+++ RFEM6_Adapter/CRUD/Create/BHoMDataStructure/Loading/LoadCombination.cs
@@ -20,78 +20,78 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
 
-//using BH.oM.Adapter;
-//using BH.oM.Structure.Elements;
-//using BH.oM.Structure.Constraints;
+using BH.oM.Adapter;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.Constraints;
 
-//using rfModel = Dlubal.WS.Rfem6.Model;
-//using BH.Engine.Structure;
-//using BH.oM.Structure.Loads;
-//using BH.oM.Adapter.Commands;
-//using Dlubal.WS.Rfem6.Model;
+using rfModel = Dlubal.WS.Rfem6.Model;
+using BH.Engine.Structure;
+using BH.oM.Structure.Loads;
+using BH.oM.Adapter.Commands;
+using Dlubal.WS.Rfem6.Model;
 
-//namespace BH.Adapter.RFEM6
-//{
-//    public partial class RFEM6Adapter
-//    {
-//        private bool CreateCollection(IEnumerable<LoadCombination> bhLoadCombinations)
-//        {
+namespace BH.Adapter.RFEM6
+{
+    public partial class RFEM6Adapter
+    {
+        //        private bool CreateCollection(IEnumerable<LoadCombination> bhLoadCombinations)
+        //        {
 
-//            // Taking Care of Analysis Settings
+        //            // Taking Care of Analysis Settings
 
-//            //Check if Analysis Setting does exist already
-//            rfModel.object_with_children[] numbers = m_Model.get_all_object_numbers_by_type(rfModel.object_types.E_OBJECT_TYPE_DESIGN_SITUATION);
-//            List<rfModel.design_situation> foundDesingSituations = numbers.ToList().Select(n => m_Model.get_design_situation(n.no)).ToList();
+        //            //Check if Analysis Setting does exist already
+        //            rfModel.object_with_children[] numbers = m_Model.get_all_object_numbers_by_type(rfModel.object_types.E_OBJECT_TYPE_DESIGN_SITUATION);
+        //            List<rfModel.design_situation> foundDesingSituations = numbers.ToList().Select(n => m_Model.get_design_situation(n.no)).ToList();
 
-//			Dictionary<int, Loadcase> LoadCaseDict = this.GetCachedOrReadAsDictionary<int, Loadcase>();
+        //			Dictionary<int, Loadcase> LoadCaseDict = this.GetCachedOrReadAsDictionary<int, Loadcase>();
 
-//			rfModel.design_situation analysis;
+        //			rfModel.design_situation analysis;
 
-//            if (foundDesingSituations.Count == 0)
-//            {
-//                int no=m_Model.get_first_free_number(rfModel.object_types.E_OBJECT_TYPE_DESIGN_SITUATION, 0);
+        //            if (foundDesingSituations.Count == 0)
+        //            {
+        //                int no=m_Model.get_first_free_number(rfModel.object_types.E_OBJECT_TYPE_DESIGN_SITUATION, 0);
 
 
-//				analysis = new design_situation()
-//                {
-//                    no = no,
-//					//name = $"DS {no}",
-//					//user_defined_name_enabled = true,
-//					//user_defined_name_enabledSpecified = true,
-//					design_situation_type = "DESIGN_SITUATION_TYPE_EQU_PERMANENT_AND_TRANSIENT",
-//					is_generated = false,
-//					is_generatedSpecified = true,
-//					consider_inclusive_exclusive_load_cases = false,
-//					consider_inclusive_exclusive_load_casesSpecified = false,
-//				};
+        //				analysis = new design_situation()
+        //                {
+        //                    no = no,
+        //					//name = $"DS {no}",
+        //					//user_defined_name_enabled = true,
+        //					//user_defined_name_enabledSpecified = true,
+        //					design_situation_type = "DESIGN_SITUATION_TYPE_EQU_PERMANENT_AND_TRANSIENT",
+        //					is_generated = false,
+        //					is_generatedSpecified = true,
+        //					consider_inclusive_exclusive_load_cases = false,
+        //					consider_inclusive_exclusive_load_casesSpecified = false,
+        //				};
 
-//                m_Model.set_design_situation(analysis);
-//			}
-//            else
-//            {
+        //                m_Model.set_design_situation(analysis);
+        //			}
+        //            else
+        //            {
 
-//                analysis = foundDesingSituations.Find(s => s.design_situation_type == "DESIGN_SITUATION_TYPE_EQU_PERMANENT_AND_TRANSIENT");
+        //                analysis = foundDesingSituations.Find(s => s.design_situation_type == "DESIGN_SITUATION_TYPE_EQU_PERMANENT_AND_TRANSIENT");
 
-//            }
+        //            }
 
-//			foreach (LoadCombination loadCombination in bhLoadCombinations)
-//			{
-//				//Convert Load Case to RFEM6
-//				rfModel.load_combination rfLoadCombination = loadCombination.ToRFEM6(analysis.no);
-//				//Set Load Case
-//				m_Model.set_load_combination(rfLoadCombination);
-//			}
+        //			foreach (LoadCombination loadCombination in bhLoadCombinations)
+        //			{
+        //				//Convert Load Case to RFEM6
+        //				rfModel.load_combination rfLoadCombination = loadCombination.ToRFEM6(analysis.no);
+        //				//Set Load Case
+        //				m_Model.set_load_combination(rfLoadCombination);
+        //			}
 
-//			return true;
-//        }
+        //			return true;
+        //        }
 
-//    }
-//}
+    }
+}
 
 
--- RFEM6_Adapter/Convert/ToRFEM6/BHoMDataStructure/Loading/LoadCombination.cs
+++ RFEM6_Adapter/Convert/ToRFEM6/BHoMDataStructure/Loading/LoadCombination.cs
@@ -19,78 +19,78 @@
  * You should have received a copy of the GNU Lesser General Public License     
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-//using BH.oM.Adapter;
-//using BH.oM.Structure.Elements;
-//using BH.Engine.Adapter;
-//using BH.oM.Adapters.RFEM6;
+using BH.oM.Adapter;
+using BH.oM.Structure.Elements;
+using BH.Engine.Adapter;
+using BH.oM.Adapters.RFEM6;
 
-//using rfModel = Dlubal.WS.Rfem6.Model;
-//using BH.Engine.Base;
-//using BH.oM.Structure.Loads;
-//using BH.oM.Adapter.Commands;
-//using Dlubal.WS.Rfem6.Model;
+using rfModel = Dlubal.WS.Rfem6.Model;
+using BH.Engine.Base;
+using BH.oM.Structure.Loads;
+using BH.oM.Adapter.Commands;
+using Dlubal.WS.Rfem6.Model;
 
-//namespace BH.Adapter.RFEM6
-//{
-//	public static partial class Convert
-//	{
+namespace BH.Adapter.RFEM6
+{
+	public static partial class Convert
+	{
 
-//		public static rfModel.load_combination ToRFEM6(this LoadCombination bhLoadCombination, int analysisNo)
-//		{
+		public static rfModel.load_combination ToRFEM6(this LoadCombination bhLoadCombination, int analysisNo)
+		{
 
-//			List<load_combination_items_row> loadCombinationItems = new List<load_combination_items_row>();
+			//			List<load_combination_items_row> loadCombinationItems = new List<load_combination_items_row>();
 
-//			int counter = 1;
-//			foreach (var loadCase in bhLoadCombination.LoadCases)
-//			{
+			//			int counter = 1;
+			//			foreach (var loadCase in bhLoadCombination.LoadCases)
+			//			{
 
-//				load_combination_items_row loadCombinationRow = new load_combination_items_row()
-//				{
-//					no = counter,
-//					row = new load_combination_items()
-//					{
-//						load_case = loadCase.Item2.Number,
-//						load_caseSpecified = true,
-//						factor = loadCase.Item1,
-//						factorSpecified = true,
-//					}
+			//				load_combination_items_row loadCombinationRow = new load_combination_items_row()
+			//				{
+			//					no = counter,
+			//					row = new load_combination_items()
+			//					{
+			//						load_case = loadCase.Item2.Number,
+			//						load_caseSpecified = true,
+			//						factor = loadCase.Item1,
+			//						factorSpecified = true,
+			//					}
 
-//				};
+			//				};
 
-//				loadCombinationItems.Add(loadCombinationRow);
+			//				loadCombinationItems.Add(loadCombinationRow);
 
-//				counter++;
+			//				counter++;
 
-//			}
-//			//TODO: Check create Load Combination
+			//			}
+			//			//TODO: Check create Load Combination
 
-//			load_combination load_Combination = new load_combination()
-//			{
-//				no = 1,
-//				name = "ScriptedCombination",
-//				user_defined_name_enabled = true,
-//				user_defined_name_enabledSpecified = true,
-//				to_solve = true,
-//				to_solveSpecified = true,
-//				analysis_type = load_combination_analysis_type.ANALYSIS_TYPE_STATIC,
-//				analysis_typeSpecified = true,
-//				items = loadCombinationItems.ToArray(),
-//				design_situation = 1,
-//				design_situationSpecified = true,
-//			};
+			//			load_combination load_Combination = new load_combination()
+			//			{
+			//				no = 1,
+			//				name = "ScriptedCombination",
+			//				user_defined_name_enabled = true,
+			//				user_defined_name_enabledSpecified = true,
+			//				to_solve = true,
+			//				to_solveSpecified = true,
+			//				analysis_type = load_combination_analysis_type.ANALYSIS_TYPE_STATIC,
+			//				analysis_typeSpecified = true,
+			//				items = loadCombinationItems.ToArray(),
+			//				design_situation = 1,
+			//				design_situationSpecified = true,
+			//			};
 
-//			return load_Combination;
+			//			return load_Combination;
 
-//		}
+		}
 
-//	}
+	}
 
-//}
+}
 
 
 
