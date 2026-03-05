41b177d7bb960f841d669813bf776e773010af26
Arne Martensen <arne.martensen@burohappold.com>
2026-03-05 09:57:39 +0100

    Fixing the RFEMNode Comparison method.

--- RFEM6_Adapter/Comparers/RFEMNodalComparer.cs
+++ RFEM6_Adapter/Comparers/RFEMNodalComparer.cs
@@ -57,7 +57,20 @@ namespace BH.Adapter.RFEM6
             Constraint6DOF c2 = node2.Support as Constraint6DOF;
 
             Constraint6DOFComparer constraint6DOFComparer = new Constraint6DOFComparer();
+            
+            var nodecomp = new NodeDistanceComparer(3);
+            
+            if (!nodecomp.Equals(node1, node2)) return false;
 
+            //Nodes are equal
+            if ((c1 is null) && (c2 is null)) return true;
+            
+            //Atelase one support is not null
+            if ((c1 is null) && !(c2 is null)) return false;
+            if (!(c1 is null) && (c2 is null)) return false;
+
+
+            //Both support are not null
             bool supportsAreEqual =
                 c1.TranslationalStiffnessX == c2.TranslationalStiffnessX
                 && c1.TranslationalStiffnessY == c2.TranslationalStiffnessY
@@ -72,7 +85,6 @@ namespace BH.Adapter.RFEM6
                 && c1.RotationY == c2.RotationY
                 && c1.RotationZ == c2.RotationZ;
 
-            var nodecomp = new NodeDistanceComparer(3);
 
             return supportsAreEqual && nodecomp.Equals(node1, node2);
         }
